c05b80d50731a582f87c3798292d683ab4bb4d31
hasanroo10 <60200112015@uin-alauddin.ac.id>
2015-06-28 17:19:12 +0800

    versi 1.0

---
+++ src/laporan3/laporan3.java
@@ -0,0 +1,15 @@
+package laporan3;
+
+public class laporan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte angka = 15;
+		byte angka2 = -15;
+		int hasil = angka++ >> 4 + 5 * 4 | 5 << angka2;
+		
+		System.out.println(hasil);
+	}
+
+}
